e288ee6a34aaa85b1801581bebd0b7d91486e4ad
AmmanRizwan <ammanrizwan00007@gmail.com>
2024-11-26 16:29:16 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,15 @@
+"""
+Display this in Terminal:
+
+
+-------------------------
+1. Add the List
+2. Update the List
+3. Delete the List
+4. Traverse the List
+5. Exit
+-------------------------
+
+"""
+
+# Use the print() function to print this message
